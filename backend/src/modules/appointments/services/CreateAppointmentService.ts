747df71d44e37952c7a62468df3d461732d21828
jwdev66 <jwdev66@gmail.com>
2021-01-14 17:34:48 -0300

    dependency inversion principle

--- backend/src/modules/appointments/services/CreateAppointmentService.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,26 +1,20 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
 
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '../repositories/AppointmentsRepository';
-
+import IAppointmentRepository from '../repositories/IAppointmentsRepository';
 interface RequestDTO {
   provider_id: string;
   date: Date;
 }
-
 class CreateAppointmentService {
-  public async execute({
-    date,
-    provider_id,
-  }: RequestDTO): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  constructor(private appointmentsRepository: IAppointmentRepository) {}
 
+  public async execute({ date, provider_id }: RequestDTO): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -28,7 +22,7 @@ class CreateAppointmentService {
       throw new AppError('This appointment is already booked.');
     }
 
-    const appointment = await appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
