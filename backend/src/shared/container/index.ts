b4fa952f27ea4cccc22f2307170d66e57d044bf5
jwdev66 <jwdev66@gmail.com>
2021-01-21 11:42:09 -0300

    saving tokens in the bank

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -9,8 +9,8 @@ import AppointmentsRepository from '@modules/appointments/infra/typeorm/reposito
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
-// import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
-// import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
+import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
+import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokensRepository';
 
 container.registerSingleton<IAppointmentRepository>(
   'AppointmentsRepository',
@@ -20,6 +20,12 @@ container.registerSingleton<IAppointmentRepository>(
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
+);
+
+
+container.registerSingleton<IUserTokensRepository>(
+  'UserTokensRepository',
+  UserTokensRepository
 );
 
 
