352d94b2b32233db11da56b284987a0a5be0f6d3
jwdev66 <jwdev66@gmail.com>
2021-02-10 18:33:01 -0300

    using class transformer

--- backend/src/modules/users/infra/http/controllers/ResetPasswordController.ts
+++ backend/src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 
 import ResetPasswordService from '@modules/users/services/ResetPasswordService';
+
 export default class ResetPasswordController {
   public async create(request: Request, response: Response): Promise<Response> {
     const { password, token } = request.body;
