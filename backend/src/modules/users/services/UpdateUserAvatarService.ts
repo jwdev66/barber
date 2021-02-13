f65719945eb071e8bcc09dcc18f1ad17e2eabd18
jwdev66 <jwdev66@gmail.com>
2021-02-13 20:47:34 -0300

    upload files to amazon ses

--- backend/src/modules/users/services/UpdateUserAvatarService.ts
+++ backend/src/modules/users/services/UpdateUserAvatarService.ts
@@ -1,14 +1,11 @@
-import path from 'path';
-import fs from 'fs';
-import uploadConfig from '@config/upload';
-
-import { injectable, inject, container } from 'tsyringe';
+import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-import IUsersRepository from '../repositories/IUsersRepository';
-import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 
+import IStorageProvider from '@shared/container/providers/StorageProvider/models/IStorageProvider';
 import User from '../infra/typeorm/entities/User';
+import IUsersRepository from '../repositories/IUsersRepository';
+
 interface IRequest {
   user_id: string;
   avatarFilename: string;
