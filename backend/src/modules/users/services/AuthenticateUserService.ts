bdf4271040d3d90dad9078a84d64299065b066eb
jwdev66 <jwdev66@gmail.com>
2021-02-15 20:22:08 -0300

    invalidating cache

--- backend/src/modules/users/services/AuthenticateUserService.ts
+++ backend/src/modules/users/services/AuthenticateUserService.ts
@@ -3,10 +3,11 @@ import authConfig from '@config/auth';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
+
 import IUsersRepository from '../repositories/IUsersRepository';
 import IHashProvider from '../providers/HashProvider/models/IHashProvider';
-
 import User from '../infra/typeorm/entities/User';
+
 interface IRequest {
   email: string;
   password: string;
@@ -34,16 +35,19 @@ class AuthenticateUserService {
     }
 
     const passwordMatched = await this.hashProvider.compareHash(
-      password, user.password
+      password,
+      user.password,
     );
 
     if (!passwordMatched) {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    const token = sign({}, authConfig.jwt.secret, {
+    const { secret, expiresIn } = authConfig.jwt;
+
+    const token = sign({}, secret, {
       subject: user.id,
-      expiresIn: '1d',
+      expiresIn,
     });
 
     return {
