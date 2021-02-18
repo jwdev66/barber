4b6c54d3411e2c5e2a562740cfcec4517bdc44cb
jwdev66 <jwdev66@gmail.com>
2021-02-17 22:10:59 -0300

    schedule listing

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { useAuth } from '../../hooks/auth';
 
 import { FiClock, FiPower } from 'react-icons/fi';
 
 import {
+  Appointment,
   Calendar,
   Container,
   Content,
@@ -12,12 +13,14 @@ import {
   HeaderContent,
   NextAppointment,
   Profile,
+  Section,
   Schedule
 } from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
 
   return (
@@ -66,6 +69,69 @@ const Dashboard: React.FC = () => {
 
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://avatars.githubusercontent.com/u/15836394?s=88&u=70673f20d41e1ee784577c36affb639d453a3552&v=4"
+                alt="Alexandre Soares" />
+
+                <strong>Alexandre Soares</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://avatars.githubusercontent.com/u/15836394?s=88&u=70673f20d41e1ee784577c36affb639d453a3552&v=4"
+                alt="Alexandre Soares" />
+
+                <strong>Alexandre Soares</strong>
+              </div>
+            </Appointment>
+          </Section>
+
+          <Section>
+            <strong>Tarde</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://avatars.githubusercontent.com/u/15836394?s=88&u=70673f20d41e1ee784577c36affb639d453a3552&v=4"
+                alt="Alexandre Soares" />
+
+                <strong>Alexandre Soares</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img src="https://avatars.githubusercontent.com/u/15836394?s=88&u=70673f20d41e1ee784577c36affb639d453a3552&v=4"
+                alt="Alexandre Soares" />
+
+                <strong>Alexandre Soares</strong>
+              </div>
+            </Appointment>
+          </Section>
+
         </Schedule>
         <Calendar />
       </Content>
