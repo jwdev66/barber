20db45fdc20768117353e2c2fd0912adb325b238
jwdev66 <jwdev66@gmail.com>
2021-02-17 21:49:31 -0300

    next appointment

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -2,8 +2,18 @@ import React from 'react';
 
 import { useAuth } from '../../hooks/auth';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiClock, FiPower } from 'react-icons/fi';
+
+import {
+  Calendar,
+  Container,
+  Content,
+  Header,
+  HeaderContent,
+  NextAppointment,
+  Profile,
+  Schedule
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -31,6 +41,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+
+            <div>
+              <img src="https://avatars.githubusercontent.com/u/15836394?s=88&u=70673f20d41e1ee784577c36affb639d453a3552&v=4"
+              alt="Alexandre Soares" />
+
+              <strong>Alexandre Soares</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+            </div>
+          </NextAppointment>
+        </Schedule>
+        <Calendar />
+      </Content>
     </Container>
   );
 }
