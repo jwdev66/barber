1d98566f24b92d30c7d9f43b7fe591166ff17d60
jwdev66 <jwdev66@gmail.com>
2021-03-20 20:41:50 -0300

    rewriting the repositories

--- backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ backend/src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,18 +1,33 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { getRepository, Repository } from 'typeorm';
 
 import IAppointmentsRespository from '@modules/appointments/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
 import Appointment from '../entities/Appointments';
+class AppointmentsRepository implements IAppointmentsRespository {
+  private ormRepository: Repository<Appointment>;
 
-@EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment>
-  implements IAppointmentsRespository {
+  constructor() {
+    this.ormRepository = getRepository(Appointment);
+  }
+
   public async findByDate(date: Date): Promise<Appointment | undefined> {
-    const findAppointment = await this.findOne({
+    const findAppointment = await this.ormRepository.findOne({
       where: { date },
     });
 
     return findAppointment;
+  }
+
+  public async create({
+    provider_id,
+    date
+  }: ICreateAppointmentDTO): Promise<Appointment> {
+    const appointment = this.ormRepository.create({ provider_id, date });
+
+    await this.ormRepository.save(appointment);
+
+    return appointment;
   }
 }
 
--- backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ backend/src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,6 +1,7 @@
 import Appointment from '../infra/typeorm/entities/Appointments';
-
+import ICreateAppointmentDTO from './../dtos/ICreateAppointmentDTO';
 interface IAppointmentsRespository {
+  create(data: ICreateAppointmentDTO): Promise<Appointment>;
   findByDate(date: Date): Promise<Appointment | undefined>;
 }
 
