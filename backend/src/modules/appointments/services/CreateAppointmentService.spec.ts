02a884ef05e687e706b9a2e0538be29eb630f874
jwdev66 <jwdev66@gmail.com>
2021-03-24 19:30:59 -0300

    scheduling tests

--- backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ backend/src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,5 @@
+import AppError from '@shared/errors/AppError';
+
 import FakeAppoinmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -15,5 +17,26 @@ describe('CreateAppointment', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('123456');
+  });
+
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppoinmentsRepository = new FakeAppoinmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppoinmentsRepository
+    );
+
+    const appointmentDate = new Date(2020, 4, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      provider_id: '123456'
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        provider_id: '123456'
+      })
+    ).rejects.toBeInstanceOf(AppError)
   });
 });
